73b507dd0c688e19e2a09b09bb6ef996170c9fd7
jparsai <jparsai@redhat.com>
2023-07-05 12:03:27 +0530

    working conversion webhook

--- api/v1alpha1/memcached_conversion.go
+++ api/v1alpha1/memcached_conversion.go
@@ -1,12 +1,15 @@
 package v1alpha1
 
 import (
+	"fmt"
+
 	"github.com/jparsai/memcached-operator/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
 // ConvertTo converts this Memcached to the Hub version (vbeta1).
 func (src *Memcached) ConvertTo(dstRaw conversion.Hub) error {
+	fmt.Println("====== ConvertTo ======")
 	dst := dstRaw.(*v1beta1.Memcached)
 	dst.Spec.ReplicaSize = src.Spec.Size
 	dst.ObjectMeta = src.ObjectMeta
@@ -16,6 +19,7 @@ func (src *Memcached) ConvertTo(dstRaw conversion.Hub) error {
 
 // ConvertFrom converts from the Hub version (vbeta1) to this version.
 func (dst *Memcached) ConvertFrom(srcRaw conversion.Hub) error {
+	fmt.Println("====== ConvertFrom ======")
 	src := srcRaw.(*v1beta1.Memcached)
 	dst.Spec.Size = src.Spec.ReplicaSize
 	dst.ObjectMeta = src.ObjectMeta
